4457192f30d9182c7cfa76695637dbaf0e30005f
ALirezapourk <alireza.pourkay@gmail.com>
2024-09-13 09:14:54 +0200

    Add project files.

---
+++ YH Upgift Library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YH_Upgift_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
